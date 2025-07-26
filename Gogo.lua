cc8fa36bcd877595390e2f0a973c4d3a454b4711
DarkscriptHUBS <yuanagayo01@gmail.com>
2025-07-26 20:22:31 +0800

    Update Gogo.lua

--- Gogo.lua
+++ Gogo.lua
@@ -1,3 +1,3 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/f4shn/main/refs/heads/main/games/gagvisual.lua", true))()
 wait.task(1)
-loadstring(game:HttpGet("https://api.rubis.app/v2/scrap/u9pWRbE1FIfU68MJ/raw"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/DarkscriptHUBS/wowk/refs/heads/main/loli"))()
